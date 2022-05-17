000224b857d987f10f867853df285c50333e4fc1
kevinnammour <nammourkevin@gmail.com>
2022-05-18 00:33:07 +0300

    sleepin

--- src/components/mobile-nav/MobileNavigation.jsx
+++ src/components/mobile-nav/MobileNavigation.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiMenuAlt3 } from "react-icons/hi";
 import "./mobile-nav.scss";
+import { motion } from "framer-motion";
+
 const MobileNavigation = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <div className="hi-menu-alt-3-wrapper">
-        <HiMenuAlt3 className="hi-menu-alt-3" />
-      </div>
+      <motion.div whileTap={{ scale: 1.1 }} whileHover={{ scale: 1.1 }} className="hi-menu-alt-3-wrapper">
+        <HiMenuAlt3 className="hi-menu-alt-3" onClick={() => setOpen(true)} />
+      </motion.div>
+      {open ? <div className="mobile-sidebar"></div> : <></>}
     </div>
   );
 };
