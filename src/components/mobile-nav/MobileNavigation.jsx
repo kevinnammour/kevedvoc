5b896b6f7e67fc028f6072c7efb57ed4a89e25d6
kevinnammour <nammourkevin@gmail.com>
2022-05-18 21:26:09 +0300

    mobile-nav

--- src/components/mobile-nav/MobileNavigation.jsx
+++ src/components/mobile-nav/MobileNavigation.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import { HiMenuAlt3 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
+import { BsPerson } from "react-icons/bs";
+import { FiCode } from "react-icons/fi";
+import { IoGitNetworkOutline } from "react-icons/io5";
+import { MdOutlineMail } from "react-icons/md";
 import "./mobile-nav.scss";
 import { motion } from "framer-motion";
 
@@ -8,10 +13,40 @@ const MobileNavigation = () => {
 
   return (
     <div>
-      <motion.div whileTap={{ scale: 1.1 }} whileHover={{ scale: 1.1 }} className="hi-menu-alt-3-wrapper">
+      <div className="hi-menu-alt-3-wrapper">
         <HiMenuAlt3 className="hi-menu-alt-3" onClick={() => setOpen(true)} />
-      </motion.div>
-      {open ? <div className="mobile-sidebar"></div> : <></>}
+      </div>
+      {open ? (
+        <motion.div
+          className="mobile-sidebar"
+          initial={{ width: 0 }}
+          animate={{ width: 90 }}
+        >
+          <div className="io-close-wrapper">
+            <IoClose className="io-close" onClick={() => setOpen(false)} />
+          </div>
+
+          <div className="mobile-nav-links-wrapper">
+            <div className="mobile-nav-link-wrapper">
+              <BsPerson className="mobile-nav-link" onClick={() => setOpen(false)} />
+            </div>
+
+            <div className="mobile-nav-link-wrapper">
+              <FiCode className="mobile-nav-link" onClick={() => setOpen(false)} />
+            </div>
+
+            <div className="mobile-nav-link-wrapper">
+              <IoGitNetworkOutline className="mobile-nav-link" onClick={() => setOpen(false)} />
+            </div>
+
+            <div className="mobile-nav-link-wrapper">
+              <MdOutlineMail className="mobile-nav-link" onClick={() => setOpen(false)} />
+            </div>
+          </div>
+        </motion.div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
