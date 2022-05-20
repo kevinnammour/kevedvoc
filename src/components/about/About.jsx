40386b48d38f61f88d0e4647e9c024558c6a8540
kevinnammour <nammourkevin@gmail.com>
2022-05-20 14:09:32 +0300

    about-us fixed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./about.scss";
 import image from "../../assets/image.webp";
 import { FaRegPaperPlane } from "react-icons/fa";
-import { IoLogoLinkedin, IoLogoGithub } from "react-icons/io5";
+import { IoLogoGithub } from "react-icons/io5";
+import { GrLinkedinOption } from "react-icons/gr";
 
 const About = () => {
   return (
     <div className="about-wrapper">
-      <div className="about-content">
+      <div className="about-content pt-3">
         <div className="desc-wrapper">
           <h1 className="desc-header">Hi, I'm Kevin Nammour</h1>
           <p className="desc-text mt-3">
@@ -20,13 +21,13 @@ const About = () => {
               <FaRegPaperPlane className="icon" />
             </button>
           </div>
-          <div className="social-media mt-5">
+          <div className="social-media mt-5 mb-4">
             <span>Check out my</span>
             <a
               href="https://www.linkedin.com/in/kevinnammour/"
               className="ms-3 me-3"
             >
-              <IoLogoLinkedin className="sm-link linkedin" />
+              <GrLinkedinOption className="sm-link linkedin" />
             </a>
             <a href="https://github.com/kevinnammour" className="me-3">
               <IoLogoGithub className="sm-link github" />
