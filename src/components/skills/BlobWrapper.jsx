5a456668588aa4afea7e4c5c29cec9c9d0471fc2
kevinnammour <nammourkevin@gmail.com>
2022-05-21 20:06:46 +0300

    added blob

--- src/components/skills/BlobWrapper.jsx
+++ src/components/skills/BlobWrapper.jsx
@@ -4,23 +4,6 @@ const BlobWrapper = () => {
   return (
     <div className="blob-wrapper">
       <h1 className="blob-header">&lt;Skills/&gt;</h1>
-      <p className="blob-text mt-3">
-        Since the beginning of my programming journey, I've collaborated with
-        many talented people to create products for both business and consumer
-        use. Through the process, I learned many technologies related to web
-        development, native and hybrid mobile app development, and much more.
-        <br />
-        <br />
-        Visit my{" "}
-        <a
-          href="https://www.linkedin.com/in/kevinnammour/?originalSubdomain=lb"
-          target={"_target"}
-          rel="noopener"
-        >
-          Linkedin
-        </a>{" "}
-        profile for more details or just <a href="">contact</a> me.
-      </p>
     </div>
   );
 };
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./skills.scss";
-import BlobWrapper from "./BlobWrapper";
 import PgLangWrapper from "./PgLangWrapper";
 
 const Skills = () => {
   return (
     <div className="skills-wrapper">
       <div className="skills-content">
-        <BlobWrapper />
+        {/* <BlobWrapper /> */}
         <PgLangWrapper />
       </div>
     </div>
