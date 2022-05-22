483878b88aa156a7ce8d5a5baf82dd55ea9f6b5e
kevinnammour <nammourkevin@gmail.com>
2022-05-22 13:41:53 +0300

    skills desc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./global.scss";
 import Menu from "./components/menu/Menu";
 import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 
 function App() {
   return (
     <>
       <Menu />
       <About />
+      <Skills />
     </>
   );
 }
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./skills.scss";
+
+const Skills = () => {
+  return (
+    <div className="skills-wrapper">
+      <div className="skills-content">
+        <h1 className="skills-header">Skills & Experience</h1>
+        <p className="skills-desc mt-3">
+          Since the beginning of my programming journey, I've collaborated with
+          many talented people to create products for both business and consumer
+          use. Through the process, I learned many technologies related to web
+          development, native and hybrid mobile app development, and much more.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
