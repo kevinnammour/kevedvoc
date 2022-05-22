fa2708b7e846542f237aacbcd58804cbcf4a4526
kevinnammour <nammourkevin@gmail.com>
2022-05-22 16:19:37 +0300

    deleted skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,13 +5,8 @@ const Skills = () => {
   return (
     <div className="skills-wrapper mt-2">
       <div className="skills-content">
-        <h1 className="skills-header">&lt;Skills/&gt;</h1>
-        <p className="skills-desc mt-3">
-          Since the beginning of my programming journey, I've collaborated with
-          many talented people to create products for both business and consumer
-          use. Through the process, I learned many technologies related to web
-          development, native and hybrid mobile app development, and much more.
-        </p>
+        <h1 className="skills-header">Skills</h1>
+        {/* <h2 className="skills-sub-header">What Can I Do Best?</h2> */}
       </div>
     </div>
   );
